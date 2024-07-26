671c2b97ca0c667dfab7b64eb2b2b631321ca6fd
TarasNovik <148384148+TarasNovik@users.noreply.github.com>
2024-07-26 21:33:37 +0200

    Update task-2.js

--- js/task-2.js
+++ js/task-2.js
@@ -6,7 +6,6 @@ function makeArray(firstArray, secondArray, maxLength) {
         return (arreyNew);
     }
 
-    return (arreyNew);
 }
 console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
 console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
